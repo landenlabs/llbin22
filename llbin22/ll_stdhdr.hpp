380dc2a25362e5fc1235cd758ad78880c566d5c2
Dennis Lang <landenlabs2@gmail.com>
2026-05-10 21:27:49 -0400

    update copyright, set version and tag o v6.05.10

--- llbin22/ll_stdhdr.hpp
+++ llbin22/ll_stdhdr.hpp
@@ -11,7 +11,7 @@
 //
 // ----- License ----
 //
-// Copyright © 2022 Dennis Lang. All rights reserved.
+// Copyright © 2026 Dennis Lang. All rights reserved.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
